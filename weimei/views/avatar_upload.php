2daca90b58d554cac816d0d340b9809504f78fd1
h2ero <122750707@qq.com>
2012-05-04 18:30:01 +0800

    add notic when upload avatar and pic

--- weimei/views/avatar_upload.php
+++ weimei/views/avatar_upload.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('header'); ?>
 
 		<div id="main">
-			<div id="inner-main">
+			<div id="upload-main">
 				<div id="left">
 					<div id="upload">	
 					<h2>发布头像</h2>
@@ -49,7 +49,11 @@
 					</div>
 				</div>
 				<!---left end--->
-				<div id="right"></div>
+				<div id="right">
+				<p class="upload-notice">
+				请勿上传违规图片，参见<a href="http://www.cnnic.net.cn/html/Dir/2000/09/25/0652.htm" target="_blank">相关法规</a>。<br><br>上传图片必须宽度小于300px,高度大于100px。一次上传可选择不超过100个文件，上传完成后可选择文件继续上传。未出现“选择文件”按钮？请安装 <a target="_blank" href="http://get.adobe.com/cn/flashplayer/">Flash Player</a><br><br><br>您所上传的图片将位于公共领域且用户不能彻底删除图片，如不确定，请勿上传私人照片。
+				</p>
+				</div>
 				<!---left end--->
 			</div>
 		</div>
--- weimei/views/pic_upload.php
+++ weimei/views/pic_upload.php
@@ -3,7 +3,7 @@ $this->load->view('header');
 ?>
 
 		<div id="main">
-			<div id="inner-main">
+			<div id="upload-main">
 				<div id="left">	
 					<div id="upload">
 						<h2>发布图片</h2>
@@ -35,8 +35,13 @@ $this->load->view('header');
 	</div>			</div>
 				</div>
 				<!---left end--->
-				<div id="right"></div>
-				<!---left end--->
+				<div id="right">
+				<p class="upload-notice">
+				请勿上传违规图片，参见<a href="http://www.cnnic.net.cn/html/Dir/2000/09/25/0652.htm" target="_blank">相关法规</a>。<br><br>上传图片必须宽度小于960px,高度大于50px。一次上传可选择不超过100个文件，上传完成后可选择文件继续上传。未出现“选择文件”按钮？请安装 <a target="_blank" href="http://get.adobe.com/cn/flashplayer/">Flash Player</a><br><br><br>您所上传的图片将位于公共领域且用户不能彻底删除图片，如不确定，请勿上传私人照片。
+				</p>
+				</div>
+			</div>
+				<!---right end--->
 			</div>
 		</div>
 	
